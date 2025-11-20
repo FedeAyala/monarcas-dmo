330cc260f137eaa6130e3367f792399c1f5abf9a
Federico Ayala <federico_ayala@ioma.gba.gov.ar>
2025-11-20 03:07:42 -0300

    Se saca vercel, se agrega netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import netlify from "@astrojs/netlify";
 
 export default defineConfig({
   output: "server",
-  adapter: vercel(),
+  adapter: netlify(),
   integrations: [react(), tailwind()],
 });
